d4d53d2f65770ad606cdd4df77ce36c7b2515dfd
sgj0402 <sgj0402@gmail.com>
2025-01-28 04:47:51 +0900

    fix: improve run hash handling message in training mode

--- global_aim.py
+++ global_aim.py
@@ -1,5 +1,4 @@
 from aim import Run
-from aim.pytorch_ignite import AimLogger
 
 from omegaconf import DictConfig
 
@@ -11,12 +10,12 @@ run = None
 def init_aim(config: DictConfig) -> None:
 
     if config.setting.mode == 'train':
-        
+
         if config.setting.train.is_resume and config.setting.aim.run_hash is None:
             raise ValueError("Resume is enabled but run_hash is not provided.")
         
         if config.setting.train.is_resume is False and config.setting.aim.run_hash is not None:
-            print("train.is_resume is False. Discarding aim.run_hash and creating a new run.")
+            print("Resume is disabled but run_hash is provided. Ignoring run_hash.")
             config.setting.aim.run_hash = None
     
     global run
