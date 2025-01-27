8b1278ed5e1a0dc171038b76d8f57d5d70346a3a
sgj0402 <sgj0402@gmail.com>
2025-01-28 04:15:58 +0900

    fix: refine run hash handling for training mode

--- global_aim.py
+++ global_aim.py
@@ -10,12 +10,14 @@ run = None
 
 def init_aim(config: DictConfig) -> None:
 
-    if config.setting.train.is_resume and config.setting.aim.run_hash is None:
-        raise ValueError("Resume is enabled but run_hash is not provided.")
-    
-    if not config.setting.train.is_resume:
-        print("train.is_resume is False. Discarding aim.run_hash and creating a new run.")
-        config.setting.aim.run_hash = None
+    if config.setting.mode == 'train':
+        
+        if config.setting.train.is_resume and config.setting.aim.run_hash is None:
+            raise ValueError("Resume is enabled but run_hash is not provided.")
+        
+        if config.setting.train.is_resume is False and config.setting.aim.run_hash is not None:
+            print("train.is_resume is False. Discarding aim.run_hash and creating a new run.")
+            config.setting.aim.run_hash = None
     
     global run
 
